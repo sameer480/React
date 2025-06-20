76d696f3af6e9dabaf15aa4964f43bf3526d6c85
unknown <sameerjawed05@gmail.com>
2025-06-21 01:23:52 +0530

    props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 import ButtonGroup from './Arrow';
+import Greeting from './Greeting';
 
 const App = () => {
   return (
     <div>
+<Greeting name="John" />
+      <h1>Welcome to the Counter App</h1>
       <ButtonGroup />
     </div>
   );
---
+++ src/Greeting.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Greeting = ({name}) => {
+  return (
+    <div>
+<h2>Hello,{name}!</h2>
+    </div>
+  )
+}
+
+export default Greeting
